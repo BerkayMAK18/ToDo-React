227aa9259b312cff0fc7e2914348200c5c840d6e
Akdemir,Berkay (HR Appr) BIP-DE-I <berkay.akdemir@boehringer-ingelheim.com>
2025-05-28 11:55:18 +0200

    Initial component Setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import './App.css';
+import Header from './components/Header'
+import Main from './components/Main'
+import User from './components/User'
+import Todos from './components/Todos'
+import Projects from './components/Projects'
+import EditTodo from './components/EditTodo'
+import Calender from './components/Calender'
+import AddNewTodo from './components/AddNewTodo'
 
 function App() {
   return (
-    <div>
-      HI
+    <div className='App'>
+      <Header>
+        <User />
+        <AddNewTodo />
+        <Calender />
+        <Projects />
+      </Header>
+      <Main>
+        <Todos />
+        <EditTodo />
+
+      </Main>
     </div>
   );
 }
