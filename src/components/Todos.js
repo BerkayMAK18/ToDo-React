0a105bc817f6ff0bfb84df826815d9aead9488e9
Akdemir,Berkay (HR Appr) BIP-DE-I <berkay.akdemir@boehringer-ingelheim.com>
2025-06-05 14:05:33 +0200

    Todo Components added + css styling

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -4,12 +4,41 @@ import Next7days from './Next7Days'
 
 function Todos({}){
 
+    const selectedProject = 'PLACEHOLDER'
 
+    const todos = [
+        {
+            id : 'd54sd4',
+            text : 'Go for a run',
+            checked : false,
+            color : '#000000',
+            project : 'personal'
+        },
+        {
+            id : 'd54fdf',
+            text : 'Meeting',
+            checked : true,
+            color : '#00ff00',
+            project : 'work'
+        },
+    ]
 
     return(
         <div className='Todos'>
-            <Todo />
-            <Next7days />
+            <div className='selected-project'>
+                {selectedProject}
+            </div>
+            <div className='todos'>
+            {
+                selectedProject === 'next 7 days' ?
+                <Next7days todos={todos} />
+                :
+                todos.map( todo =>
+                    <Todo todo={todo} key={todo.key} />
+                )
+            }
+
+            </div>
         </div>
 
     )
